15dda94009789812dbcb6b6d55085ab687e0ecf1
Rehan Kedia <r_kedia@u.pacific.edu>
2021-04-01 14:55:00 +0530

    Added code to get error message back from the terminal after compiling the code

--- server_side/server.js
+++ server_side/server.js
@@ -21,6 +21,17 @@ app.get('/backend',(req, res) => {
 app.post('/backend', (req, res) => {
     console.log(req.body.code);
     writeStream.write(req.body.code); 
+    exec("gcc code_to_compile.c", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
     var serResponse={
         name: "Code Received"
     }
